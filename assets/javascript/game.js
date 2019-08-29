becac8dafd9c008c104f5eab66c7c76ecf0d21bd
ChrisM <chrismikstas@gmail.com>
2019-08-28 21:23:20 -0600

    game working within console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,64 @@ var guessesMade = [];
 
 var compGuessIndex = 0;
 
-//** Function to start the game **/
+//** Function to have the browser generate a letter for the game **/
 function init(){
 
-compGuessIndex = Math.floor(Math.random() * 26);
-console.log(compGuessIndex);
-
+    compGuessIndex = Math.floor(Math.random() * 26);
+    console.log(compGuess[compGuessIndex]);
+    guessLeft = 9;
+    guessesMade = [];
 }
 
-document.onkeyup = function(){
+//** Function to begin guessing letters **/
+document.onkeyup = function(event){
 
+/**code to log what key was hit**/
+    var key = event.key;
+    console.log(key);
+
+/**Code to convert to lower case**/
+    key = key.toLowerCase();
+
+/**Code to check for valid keyed entry**/
+    var isValid = false;
+    for (i = 0; i < compGuess.length; i++){
+        if (key === compGuess[i]){
+            isValid = true;
+        }
+        
+    }
+
+    if (!isValid){
+        console.log("invalid");
+        return;
+    }
+
+    for (i = 0; i < guessesMade.length; i++){
+        if (key === guessesMade[i]){
+            console.log("Already guessed");
+            return;
+        }
+    }
+
+//**Code to check if entry matched computers**//
+    if (key === compGuess[compGuessIndex]){
+        
+        win++;
+        console.log("Wins " + win + ", Losses " + loss);
+        init();
+
+    }
+    else {
+        console.log("miss");
+        guessLeft--;
+        console.log("guessLeft " + guessLeft);
+        guessesMade.push(key);
+        console.log("Values guessed " + guessesMade);
+        if (guessLeft === 0){
+            loss++;
+            console.log("Wins " + win + ", Losses " + loss);
+            init();
+        }
+    }
 }
